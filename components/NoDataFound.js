1d9d95db8416553bcc1430fbdc14d18b59b72eeb
mishraroushan <60612629+mishraroushan@users.noreply.github.com>
2023-09-30 11:54:14 +0530

    Update NoDataFound.js

--- components/NoDataFound.js
+++ components/NoDataFound.js
@@ -1,7 +1,7 @@
 export default function NoDataFound(){
     return(
         <div className="text-center">
-          <p className="text-white  text-sm sm:text-md md:text-lg lg:text-xl xl:text-2xl  p-5 font-serif">
+          <p className="text-black text-sm sm:text-md md:text-lg lg:text-xl xl:text-2xl p-5 font-serif">
               No Data Found!!
            </p>
         </div>
