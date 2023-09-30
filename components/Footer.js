91e67ea7d79a62789092ee8738de3adf63168d10
mishraroushan <60612629+mishraroushan@users.noreply.github.com>
2023-09-30 13:33:37 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -35,22 +35,23 @@ export default function Footer(){
               </svg>
             </Link>
             <Link
-              href="#!"
-              className="mr-6  text-black hover:text-white"
+              href="https://youtu.be/EQuEHmyWK2A"
+              className="mr-6 text-black hover:text-white"
+             target="_blank"
+             rel="noopener noreferrer"
+             >
+            <svg
+             xmlns="http://www.w3.org/2000/svg"
+             className="h-5 w-5"
+              fill="currentColor"
+              viewBox="0 0 24 24"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  d="M7 11v2.4h3.97c-.16 1.029-1.2 3.02-3.97 3.02-2.39 0-4.34-1.979-4.34-4.42 0-2.44 1.95-4.42 4.34-4.42 1.36 0 2.27.58 2.79 1.08l1.9-1.83c-1.22-1.14-2.8-1.83-4.69-1.83-3.87 0-7 3.13-7 7s3.13 7 7 7c4.04 0 6.721-2.84 6.721-6.84 0-.46-.051-.81-.111-1.16h-6.61zm0 0 17 2h-3v3h-2v-3h-3v-2h3v-3h2v3h3v2z"
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </Link>
+           <path
+            d="M21.58 7.19s-.2-1.391-.815-2.003c-.756-.797-1.514-.8-1.879-.847-2.63-.19-6.576-.19-6.576-.19h-.006s-3.947 0-6.576.19c-.366.047-1.123.05-1.879.847-.615.612-.815 2.003-.815 2.003s-.26 1.644-.26 3.29v1.044c0 1.644 .26 3.29 .26 3.29s .2 1.391 .815 2.003c .756 .797 1.751 .771 2.19 .855 1.6 .153 6.49 .195 6.6 .195s3.945-.006 6.576-.195c .366-.047 1.123-.05 1.879-.847 .615-.612 .815-2.003 .815-2.003s .26-1.644 .26-3.29v-1.044c-.006-1.644-.266-3.29-.266-3.29zm-14.41 5.503l-.005-4.8 4.76 2.396-4.755 2.404z"
+          />
+          </svg>
+          </Link>
+
             <Link
               href="#!"
               className="mr-6  text-black hover:text-white"
@@ -65,8 +66,10 @@ export default function Footer(){
               </svg>
             </Link>
             <Link
-              href="#!"
-              className="mr-6  text-black hover:text-white"
+              href="https://www.linkedin.com/in/dumping-data-4355b2281"
+              className="mr-6 text-black hover:text-white"
+              target="_blank"
+              rel="noopener noreferrer"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -90,7 +93,7 @@ export default function Footer(){
               <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
               <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
             </svg>
-            New York, NY 10012, US
+            603, 6th Floor, Platinum Arcade, Girgaon, Charniroad, Mumbai - 400004
           </div>
           <div className="mb-4 flex items-center justify-center md:justify-center text-xs sm:text-sm md:text-md lg:text-lg xl:text-xl font-base">
             <svg
@@ -102,7 +105,7 @@ export default function Footer(){
               <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
               <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
             </svg>
-            info@example.com
+            sales@dumpingdata.com
           </div>
           <div className="mb-4 flex items-center justify-center md:justify-center text-xs sm:text-sm md:text-md lg:text-lg xl:text-xl font-base">
             <svg
@@ -117,7 +120,7 @@ export default function Footer(){
                 clipRule="evenodd"
               />
             </svg>
-            + 01 234 567 88
+            Phone No.: +91 22 3544 0530,  +91 77389 05847
           </div>
         </div>
 
