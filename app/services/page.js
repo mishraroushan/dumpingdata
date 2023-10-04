a6feb32bce8aac5c669b1012e9fdbb2d57a4b8c1
mishraroushan <60612629+mishraroushan@users.noreply.github.com>
2023-10-04 15:31:45 +0530

    Update page.js

--- app/services/page.js
+++ app/services/page.js
@@ -6,10 +6,10 @@ export default function Services(){
        <p className="font-serif text-white p-5 text-center text-xl sm:text2xl md:text-3xl lg:text-4xl xl:text-5xl md:py-20 bg-center "style={{ backgroundImage: 'url("services.jpg")',}}>
         Our Services
       </p>
-        <p className="font-serif text-black  p-5 text-center text-md sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
+        <p className="font-serif text-black bg-orange-400 p-5 text-center text-md sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
         Dumping Data is a web-based platform that provides a wide range of services to exporters and importers in India.
         </p>
-        <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 p-4 md:px-20 xl:px-40 bg-cover" style={{ backgroundImage: 'url("contact-bg.jpg")' }}>
+        <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 p-4 md:px-20 xl:px-40 bg-cover" style={{ backgroundImage: 'url("service.png")' }}>
         {Service.map((data) => (
           <div className="relative p-2 bg-white border border-gray-200 rounded-lg shadow-lg hover:bg-orange-300 " key={data.title}>
             <p className="text-black  text-xs sm:text-sm md:text-md lg:text-lg xl:text-xl  text-center font-bold font-serif">
