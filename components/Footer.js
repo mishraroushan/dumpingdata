831054f93f7b84a5be161289e34d7263ec720964
mishraroushan <60612629+mishraroushan@users.noreply.github.com>
2023-10-04 12:54:02 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -95,19 +95,7 @@ export default function Footer(){
             </svg>
             603, 6th Floor, Platinum Arcade, Girgaon, Charniroad, Mumbai - 400004
           </div>
-          <div className="mb-4 flex items-center justify-center md:justify-center text-xs sm:text-sm md:text-md lg:text-lg xl:text-xl font-base">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="mr-3 h-5 w-5"
-            >
-              <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
-              <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
-            </svg>
-            sales@dumpingdata.com
-          </div>
-          <div className="mb-4 flex items-center justify-center md:justify-center text-xs sm:text-sm md:text-md lg:text-lg xl:text-xl font-base">
+                          <div className="mb-4 flex items-center justify-center md:justify-center text-xs sm:text-sm md:text-md lg:text-lg xl:text-xl font-base">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -121,6 +109,18 @@ export default function Footer(){
               />
             </svg>
             Phone No.: +91 22 3544 0530,  +91 77389 05847
+          </div>
+          <div className="mb-4 flex items-center justify-center md:justify-center text-xs sm:text-sm md:text-md lg:text-lg xl:text-xl font-base">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="mr-3 h-5 w-5"
+            >
+              <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
+              <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
+            </svg>
+            sales@dumpingdata.com
           </div>
         </div>
 
